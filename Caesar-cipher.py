4083bf3eef725e1ea6be50108f4c121d67727d5e
Nedal <44215261+CyberSy@users.noreply.github.com>
2018-10-17 15:20:31 +0300

    Update Caesar-cipher.py

--- Caesar-cipher.py
+++ Caesar-cipher.py
@@ -1,14 +1,15 @@
-# Ceaser Cipher script by CyberSy
+#    Ceaser Cipher script by CyberSy
 # https://en.wikipedia.org/wiki/Caesar_cipher
 
 # the string to be encrypted/decrypted:
 message = input ('Enter your message: ')
 
 # the encryption/decryptin key 
-key = 1
+key = input ("Enter The Encryption Key: ")
 
 # tells the program to encrypt or decrypt 
-mode = 'encrypt' # set to "encrypt" or "decrypt"
+print (" [+] Chose a Mode: \n [1] Encrypt \n [2] Decrypt ")
+mode = input ("Enter The Mode: ") # set to "encrypt" or "decrypt"
 
 # every possible symbol that can be encrypted 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -18,16 +19,16 @@ translated = ''
 
 # capitalize the string in message 
 message = message.upper()
-
+    
 # run the encryption/decryption code on each words in the message string 
 for words in message:
     if words in LETTERS:
         # get the encrypted (or decrypted) number for this words
         num = LETTERS.find(words) # get the number of the words
-        if mode == 'encrypt':
-            num = num + key 
-        elif mode == 'decrypt':
-            num = num - key 
+        if mode == 'Encrypt':
+            num = num + int(key)
+        elif mode == 'Decrypt':
+            num = num - int(key) 
 
         # handle the warp-around if num i larger than the length of 
         # LETTERS or less than 0
@@ -43,5 +44,4 @@ for words in message:
          # just add the words without encryption/decryption 
          translated = translated + words
 
-print(translated)
- 
+print("[+] Your Encrypted Message Is: " + translated)
